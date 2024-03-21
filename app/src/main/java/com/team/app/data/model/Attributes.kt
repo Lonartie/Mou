ebc4516fa8e0653ae53302f9157dc4e8106915f2
lonartie <gierschner.lg@gmail.com>
2024-03-21 11:01:07 +0100

    added attributes model

---
+++ app/src/main/java/com/team/app/data/model/Attributes.kt
@@ -0,0 +1,8 @@
+package com.team.app.data.model
+
+data class Attributes(
+    val coins: Int,
+    val hunger: Int, // 0 - 100
+    val happiness: Int, // 0 - 100
+    val health: Int, // 0 - 100
+)
