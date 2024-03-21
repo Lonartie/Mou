33b2141687559c756b1e15fade10fc96686a93dd
Matthias <matthias.neubauer8@gmail.com>
2024-03-21 15:28:15 +0100

    Add Mou Figure with control code

--- app/src/main/java/com/team/app/ui/home/HomePage.kt
+++ app/src/main/java/com/team/app/ui/home/HomePage.kt
@@ -2,16 +2,12 @@
 
 package com.team.app.ui.home
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -19,52 +15,40 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.materialIcon
-import androidx.compose.material.icons.rounded.Architecture
 import androidx.compose.material.icons.rounded.ChildCare
 import androidx.compose.material.icons.rounded.ControlPoint
 import androidx.compose.material.icons.rounded.Fastfood
-import androidx.compose.material.icons.rounded.Games
 import androidx.compose.material.icons.rounded.Home
-import androidx.compose.material.icons.rounded.MonetizationOn
-import androidx.compose.material.icons.rounded.ShoppingBasket
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.BottomAppBarDefaults
-import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import coil.compose.AsyncImagePainter.State.Empty.painter
 import com.team.app.R
-import com.team.app.data.model.Attributes
 
 @Preview
 @Composable
@@ -92,7 +76,12 @@ fun TopRow(viewModel: HomePageViewModel = hiltViewModel()) {
     TopAppBar(title = {
         Row(
             modifier = Modifier
-                .padding(10.dp, 0.dp, 20.dp, 0.dp)
+                .padding(
+                    10.dp,
+                    0.dp,
+                    20.dp,
+                    0.dp
+                )
                 .fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
@@ -156,6 +145,7 @@ fun TopRow(viewModel: HomePageViewModel = hiltViewModel()) {
 @Preview
 @Composable
 fun Content(innerPadding: PaddingValues = PaddingValues(0.dp), viewModel: HomePageViewModel = hiltViewModel()) {
+    /*
     Text(
         modifier = Modifier
             .fillMaxSize()
@@ -165,6 +155,30 @@ fun Content(innerPadding: PaddingValues = PaddingValues(0.dp), viewModel: HomePa
         textAlign = TextAlign.Center,
         fontSize = 20.sp
     )
+
+     */
+    Background(image = R.drawable.background_evening,
+               modifier = Modifier
+                   .fillMaxSize()
+                   .scale(5f))
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .offset(y = 190.dp)
+    ) {
+        Figure(
+            image = viewModel.figureState.intValue,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        Ground(
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+
+    }
 }
 
 @Preview
@@ -176,7 +190,12 @@ fun BottomRow(viewModel: HomePageViewModel = hiltViewModel()) {
             modifier = Modifier
                 .fillMaxHeight()
                 .fillMaxWidth()
-                .padding(20.dp, 0.dp, 20.dp, 0.dp)
+                .padding(
+                    20.dp,
+                    0.dp,
+                    20.dp,
+                    0.dp
+                )
         ) {
             NavigationButton(
                 name = "Food",
@@ -286,4 +305,31 @@ fun NavigationButton(
             fontSize = 18.sp
         )
     }
+}
+
+@Composable
+fun Figure(image : Int, modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = image),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Ground(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.ground),
+        contentDescription = null,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Background(image: Int, modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = image),
+        contentDescription = null,
+        modifier = modifier
+    )
 }
