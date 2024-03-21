340299fc3b496f138c845b4500046c1353e8d6e7
Matthias <matthias.neubauer8@gmail.com>
2024-03-21 15:28:15 +0100

    Add Mou Figure with control code

--- app/src/main/java/com/team/app/ui/home/HomePageViewModel.kt
+++ app/src/main/java/com/team/app/ui/home/HomePageViewModel.kt
@@ -1,14 +1,37 @@
 package com.team.app.ui.home
 
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.team.app.R
 import com.team.app.data.model.Attributes
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlin.math.min
 
 @HiltViewModel
 class HomePageViewModel @Inject constructor() : ViewModel() {
-    val attributes = mutableStateOf(Attributes(20, 90, 75, 14))
+    val attributes = mutableStateOf(Attributes(20, 90, 90, 40))
+    val figureState = mutableIntStateOf(R.drawable.figure_happy)
+
+    private fun getMinimalAttributeValue(): Int {
+        return min(
+            attributes.value.hunger,
+            min(
+                attributes.value.happiness,
+                attributes.value.health
+            )
+        )
+
+    }
+    fun setFigureState() {
+        val minimalValue = getMinimalAttributeValue()
+        figureState.intValue = when {
+            minimalValue >= 75 -> R.drawable.figure_happy
+            minimalValue >= 50 -> R.drawable.figure_lonely
+            else -> R.drawable.figure_angry
+        }
+    }
 
     fun openMoneyScreen() {
         println("Open money screen")
