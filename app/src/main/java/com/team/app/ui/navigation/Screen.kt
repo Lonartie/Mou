0566f3b46f449467fab3b48c9de239a86daa547e
wbrrobin <weber_ro@gmx.de>
2024-03-22 10:28:15 +0100

    NavHost changes

--- app/src/main/java/com/team/app/ui/navigation/Screen.kt
+++ app/src/main/java/com/team/app/ui/navigation/Screen.kt
@@ -5,9 +5,29 @@ import androidx.navigation.NavType
 import androidx.navigation.navArgument
 
 sealed class Screen(
-    val route: String
+    val route: String,
+    val navArguments: List<NamedNavArgument> = emptyList()
 ) {
-    data object Home : Screen("home")
+    data object Home : Screen(
+        route = "home/{foodId}/{toyId}/{itemId}",
+        navArguments = listOf(
+            navArgument("foodId") { type = NavType.IntType },
+            navArgument("toyId") { type = NavType.IntType },
+            navArgument("itemId") { type = NavType.IntType }
+        )
+    ) {
+        fun createRoute(
+            foodId: Int,
+            toyId: Int,
+            itemId: Int
+        ) = "home/{foodId}/{toyId}/{itemId}"
+    }
 
     data object Shop : Screen(route = "shop")
+
+    data object Food : Screen(route = "food")
+
+    data object Toys : Screen(route = "toys")
+
+    data object Items : Screen(route = "items")
 }
