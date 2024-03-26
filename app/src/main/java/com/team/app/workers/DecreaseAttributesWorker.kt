7e19a048cd33a1e62b4ba482d948fc3561f8905b
lonartie <gierschner.lg@gmail.com>
2024-03-26 15:58:56 +0100

    fix build

--- app/src/main/java/com/team/app/workers/DecreaseAttributesWorker.kt
+++ app/src/main/java/com/team/app/workers/DecreaseAttributesWorker.kt
@@ -2,6 +2,7 @@ package com.team.app.workers
 
 import android.content.Context
 import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
@@ -10,6 +11,10 @@ import dagger.assisted.AssistedInject
 class DecreaseAttributesWorker @AssistedInject constructor(
     @Assisted appContext: Context,
     @Assisted workerParams: WorkerParameters,
-){
+) : CoroutineWorker(appContext, workerParams) {
+
+    override suspend fun doWork(): Result {
+        TODO("Not yet implemented")
+    }
 
 }
