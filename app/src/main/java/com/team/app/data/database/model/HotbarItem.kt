575675f622308a4df45d80ced98af4bdcc494e4c
lonartie <gierschner.lg@gmail.com>
2024-03-26 09:31:19 +0100

    inventory database fix

--- app/src/main/java/com/team/app/data/database/model/HotbarItem.kt
+++ app/src/main/java/com/team/app/data/database/model/HotbarItem.kt
@@ -4,27 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(
-    tableName = "hotbar",
-    foreignKeys = [
-        androidx.room.ForeignKey(
-            entity = InventoryItem::class,
-            parentColumns = ["id"],
-            childColumns = ["food"],
-            onUpdate = androidx.room.ForeignKey.CASCADE,
-        ),
-        androidx.room.ForeignKey(
-            entity = InventoryItem::class,
-            parentColumns = ["id"],
-            childColumns = ["toy"],
-            onUpdate = androidx.room.ForeignKey.CASCADE,
-        ),
-        androidx.room.ForeignKey(
-            entity = InventoryItem::class,
-            parentColumns = ["id"],
-            childColumns = ["misc"],
-            onUpdate = androidx.room.ForeignKey.CASCADE,
-        )
-    ]
+    tableName = "hotbar"
 )
 data class HotbarItem(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
--- app/src/main/java/com/team/app/data/database/model/InventoryItem.kt
+++ app/src/main/java/com/team/app/data/database/model/InventoryItem.kt
@@ -4,15 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(
-    tableName = "inventory",
-    foreignKeys = [
-        androidx.room.ForeignKey(
-            entity = Item::class,
-            parentColumns = ["id"],
-            childColumns = ["itemID"],
-            onUpdate = androidx.room.ForeignKey.CASCADE,
-        )
-    ]
+    tableName = "inventory"
 )
 data class InventoryItem(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
